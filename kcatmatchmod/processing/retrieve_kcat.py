b959363c8b2be9730b324a7d60b548dd48ff6d34
hugues_esc <hugues.escoffier@gmail.com>
2025-07-24 11:35:43 +0200

    reformat repository

---
+++ kcatmatchmod/processing/retrieve_kcat.py
@@ -0,0 +1,17 @@
+
+
+def retrieve_kcat_from_brenda(): 
+    pass 
+
+
+def retrieve_kcat_from_sabio_rk():
+    pass
+
+
+def retrieve_kcat_from_catapro(): 
+    pass 
+
+
+def retrieve_kcat_from_turnip():
+    pass
+
