d23dd6e54c8a53fdc04607959bf0b7c72d48f03e
hugues_esc <hugues.escoffier@gmail.com>
2025-12-08 17:50:34 +0100

    version bump

--- wildkcat/cli.py
+++ wildkcat/cli.py
@@ -6,7 +6,7 @@ from wildkcat import run_extraction, run_retrieval, run_prediction_part1, run_pr
 
 load_dotenv()
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 
 app = typer.Typer(help="WILDkCAT CLI - Extract, Retrieve and Predict kcat values for a metabolic model.")
 
